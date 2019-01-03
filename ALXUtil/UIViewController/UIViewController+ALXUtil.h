3a8f3e5bc950a71334a776fa8495b069b65ec805
Alexgao <229570121@qq.com>
2019-01-03 16:54:17 +0800

    补充VC类别-上下安全区

--- ALXUtil/UIViewController/UIViewController+ALXUtil.h
+++ ALXUtil/UIViewController/UIViewController+ALXUtil.h
@@ -11,6 +11,18 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UIViewController (ALXUtil)
 
+#pragma mark - Layout
+/**
+ 注意：导航栏什么的不是一开始就有的
+ ios11以前：没有导航栏时是20，有导航栏时是44；ios11及以后：用.view.safeAreaInsets.top
+ */
+@property (nonatomic, readonly) CGFloat alx_topSafeArea;
+/**
+ 注意：tabBar什么的不是一开始就有的
+ ios11以前：没有tabBar时是0，有tabBar时时49；ios11及以后：用.view.safeAreaInsets.bottom
+ */
+@property (nonatomic, readonly) CGFloat alx_bottomSafeArea;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- ALXUtil/UIViewController/UIViewController+ALXUtil.m
+++ ALXUtil/UIViewController/UIViewController+ALXUtil.m
@@ -9,4 +9,32 @@
 
 @implementation UIViewController (ALXUtil)
 
+/**
+ 注意：导航栏什么的不是一开始就有的
+ ios11以前：没有导航栏时是20，有导航栏时是44；ios11及以后：用.view.safeAreaInsets.top
+ */
+- (CGFloat)alx_topSafeArea {
+    
+    if (@available(iOS 11.0, *)) {
+        return self.view.safeAreaInsets.top;
+    } else {
+        return self.topLayoutGuide.length;
+    }
+}
+/**
+ 注意：tabBar什么的不是一开始就有的
+ ios11以前：没有tabBar时是0，有tabBar时时49；ios11及以后：用.view.safeAreaInsets.bottom
+ */
+- (CGFloat)alx_bottomSafeArea {
+    
+    if (@available(iOS 11.0, *)) {
+        return self.view.safeAreaInsets.bottom;
+    } else {
+        if (self.hidesBottomBarWhenPushed) {
+            return 0;
+        }
+        return self.bottomLayoutGuide.length;
+    }
+}
+
 @end
